c013a5a652db5073c0811122538c003ea5fdeb09
casfoole <casfoole2000@gmail.com>
2017-09-15 12:41:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+var james   = 1;
+var liam    = 2;
+var aiden   = 3;
+var lewis   = 4;
+var nigel   = 5;
+var harry   = 'Alpha';
+var jaimy   = '1200';
+var Kane    = 'Chalie';
+var john    = 'Delta';
+var denzel  = 'Echo';
+var jazz    = true;
+var henry   = true;
+var ace     = true;
+var cheese  = false;
+var foxtrot = false;
+
+console.log("voor verandering var james = " + james);
+james = 8
+console.log("na verandering var james = " + james);
+console.log("voor verandering var liam = " + liam);
+liam = 12
+console.log("na verandering var liam = " + liam);
+
+var div = document.getElementById('show')
+div.innerHTML = typeof(ace)
+
+var coersion1 = james + liam + ace;
+console.log(coersion1);
+
+harry = Number(harry);
+var coersion2 = aiden + harry;
+console.log(coersion2);
